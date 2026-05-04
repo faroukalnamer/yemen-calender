af5df517cf7d3bebb4d2379deac1cbe3fe590808
Farouk Alnamer <faroukalnamer@gmail.com>
2026-05-04 16:33:59 +0300

    Create manifest.js

---
+++ manifest.js
@@ -0,0 +1,15 @@
+{
+  "name": "التقويم اليمني",
+  "short_name": "تقويم يمني",
+  "start_url": "/",
+  "display": "standalone",
+  "background_color": "#ffffff",
+  "theme_color": "#ce1126",
+  "icons": [
+    {
+      "src": "/icon-512.png",
+      "sizes": "512x512",
+      "type": "image/png"
+    }
+  ]
+}
